37a59ceab9fee7f78694668d4c9606fc5ebcb78c
Adam Harris <aharris88@users.noreply.github.com>
2015-05-28 13:57:23 -0600

    Add chart

--- chart.js
+++ chart.js
@@ -1,4 +1,30 @@
-d3.chart('boggle-down', function() {
-	
+d3.chart('boggle-down', {
+  initialize: function () {
+    'use strict';
+
+    var _Chart = this;
+
+    this._layer = this.base.append('g');
+
+    _Chart.layer('bars', this._layer, {
+      dataBind: function (data) {
+        return this.selectAll('.letterGroup')
+          .data(data);
+      },
+      insert: function () {
+        return this.append('g')
+	  .classed('letterGroup', true);
+      }
+    })
+    .on('enter', function () {
+      return this;
+    })
+    .on('merge', function () {
+      return this;
+    })
+    .on('exit', function () {
+      return this.remove();
+    });
+  }
 });
 
