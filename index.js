e9ff587743ab5f8317a636b916074811dcdb5426
aharris88 <aharris88@gmail.com>
2015-05-28 17:31:38 -0600

    Add fake data

--- index.js
+++ index.js
@@ -8,44 +8,119 @@ var svg = d3.select('#game')
 var chart = svg.append("g")
 	.chart('boggle-down');
 
-var dice = [
-  'A', 'A', 'E', 'E', 'G', 'N',
-  'E', 'L', 'R', 'T', 'T', 'Y',
-  'A', 'O', 'O', 'T', 'T', 'W',
-  'A', 'B', 'B', 'J', 'O', 'O',
-  'E', 'H', 'R', 'T', 'V', 'W',
-  'C', 'I', 'M', 'O', 'T', 'U',
-  'D', 'I', 'S', 'T', 'T', 'Y',
-  'E', 'I', 'O', 'S', 'S', 'T',
-  'D', 'E', 'L', 'R', 'V', 'Y',
-  'A', 'C', 'H', 'O', 'P', 'S',
-  'H', 'I', 'M', 'N', 'Qu','U',
-  'E', 'E', 'I', 'N', 'S', 'U',
-  'E', 'E', 'G', 'H', 'N', 'W',
-  'A', 'F', 'F', 'K', 'P', 'S',
-  'H', 'L', 'N', 'N', 'R', 'Z',
-  'D', 'E', 'I', 'L', 'R', 'X'
+var data = [
+  {
+    id: 0,
+    value: 'A',
+    row: 0,
+    column: 0,
+    color: 'red'
+  },
+  {
+    id: 1,
+    value: 'W',
+    row: 0,
+    column: 1,
+    color: 'green'
+  },
+  {
+    id: 2,
+    value: 'E',
+    row: 0,
+    column: 2,
+    color: 'orange'
+  },
+  {
+    id: 3,
+    value: 'S',
+    row: 0,
+    column: 3,
+    color: 'red'
+  },
+  {
+    id: 4,
+    value: 'Qu',
+    row: 1,
+    column: 0,
+    color: 'red'
+  },
+  {
+    id: 5,
+    value: 'F',
+    row: 1,
+    column: 1,
+    color: 'red'
+  },
+  {
+    id: 6,
+    value: 'T',
+    row: 1,
+    column: 2,
+    color: 'red'
+  },
+  {
+    id: 7,
+    value: 'H',
+    row: 1,
+    column: 3,
+    color: 'red'
+  },
+  {
+    id: 8,
+    value: 'O',
+    row: 2,
+    column: 0,
+    color: 'red'
+  },
+  {
+    id: 9,
+    value: 'N',
+    row: 2,
+    column: 1,
+    color: 'red'
+  },
+  {
+    id: 10,
+    value: 'M',
+    row: 2,
+    column: 2,
+    color: 'red'
+  },
+  {
+    id: 11,
+    value: 'W',
+    row: 2,
+    column: 3,
+    color: 'red'
+  },
+  {
+    id: 12,
+    value: 'I',
+    row: 3,
+    column: 0,
+    color: 'red'
+  },
+  {
+    id: 13,
+    value: 'P',
+    row: 3,
+    column: 1,
+    color: 'red'
+  },
+  {
+    id: 14,
+    value: 'G',
+    row: 3,
+    column: 2,
+    color: 'red'
+  },
+  {
+    id: 15,
+    value: 'H',
+    row: 3,
+    column: 3,
+    color: 'red'
+  }
 ];
-
-
-var data = [];
-
-var numberOfRows = 4;
-var extraRows = 2;
-var numberOfColumns = 4;
-var randomNumber;
-
-var totalRows = numberOfRows + extraRows;
-for (var i = 0; i < totalRows * numberOfColumns; i += 1) {
-	row = Math.floor(i / numberOfColumns);
-	column = i % numberOfColumns;
-	randomNumber = Math.floor(Math.random() * dice.length);
-
-	if (column === 0) {
-		data.push([]);
-	}
-	data[row][column] = dice[randomNumber];
-}
-
 
 chart.draw(data);
