8b38085f7b77e47f32980c887e8c80b8780bcf95
Pablo Arcia <pablo.arcia@domo.com>
2015-05-28 19:13:45 -0600

    Each tile now has its own letter

--- chart.js
+++ chart.js
@@ -6,6 +6,7 @@ d3.chart('boggle-down', {
     _Chart.height= 600;
     var padding = 10;
     var tileSize = (_Chart.height / 5) - padding;
+    var fontSize = tileSize * 0.6;
     var yPos = function(d) { return _Chart.height - tileSize - (d.row * (tileSize + padding)); };
     var xPos = function(d) { return d.column * (tileSize + padding); };
 
@@ -24,6 +25,8 @@ d3.chart('boggle-down', {
       }
     })
     .on('enter', function () {
+      
+      
       this.append('rect')
           .attr({
             'class': 'tiles',
@@ -31,18 +34,40 @@ d3.chart('boggle-down', {
             'width': tileSize,
             'rx': 15,
             'ry': 15
-            })
+            });
+            
+      this.append('text')
+          .attr({
+            'class': 'letter',
+            'font-size': fontSize,
+            'text-anchor': 'middle'
+          });  
+           
       return this;
     })
     .on('merge', function () {
-      this.style('fill', function(d) {
-            return d.color;
-          })
-          .attr({
+      
+      this.attr({
             'transform': function(d) { 
               return 'translate(' + xPos(d) + ',' + yPos(d)+ ')';
              }
           })
+      
+      this.select('rect.tiles')
+      .style('fill', function(d) {
+            return d.color;
+          })
+          ;
+      
+      this.select('text.letter')
+          .style({
+            'fill': '#fff'
+          })
+          .attr({
+            'dy': tileSize * 0.7,
+            'dx': tileSize * 0.5
+          })
+          .text(function(d) { return d.value; });
       return this;
     })
     .on('exit', function () {
