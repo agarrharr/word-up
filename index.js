cbe4141defeb7be6fb45894840c3ffdd81b12454
aharris88 <aharris88@gmail.com>
2015-05-28 19:39:54 -0600

    Add setTimeout to test transitions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var score = 0;
 var highScore = 19;
+var savedColors = [];
 
 var svg = d3.select('#game')
 	.append('svg')
@@ -17,116 +18,346 @@ var data = [
     value: 'W',
     row: 0,
     column: 0,
-    color: getRandomColor()
+    color: getRandomColor(0)
   },
   {
     id: 1,
     value: 'O',
     row: 0,
     column: 1,
-    color: getRandomColor()
+    color: getRandomColor(1)
   },
   {
     id: 2,
     value: 'T',
     row: 0,
     column: 2,
-    color: getRandomColor()
+    color: getRandomColor(2)
   },
   {
     id: 3,
     value: 'Qu',
     row: 0,
     column: 3,
-    color: getRandomColor()
+    color: getRandomColor(3)
   },
   {
     id: 4,
     value: 'L',
     row: 1,
     column: 0,
-    color: getRandomColor()
+    color: getRandomColor(4)
   },
   {
     id: 5,
     value: 'S',
     row: 1,
     column: 1,
-    color: getRandomColor()
+    color: getRandomColor(5)
   },
   {
     id: 6,
     value: 'R',
     row: 1,
     column: 2,
-    color: getRandomColor()
+    color: getRandomColor(6)
   },
   {
     id: 7,
     value: 'A',
     row: 1,
     column: 3,
-    color: getRandomColor()
+    color: getRandomColor(7)
   },
   {
     id: 8,
     value: 'Y',
     row: 2,
     column: 0,
-    color: getRandomColor()
+    color: getRandomColor(8)
   },
   {
     id: 9,
     value: 'E',
     row: 2,
     column: 1,
-    color: getRandomColor()
+    color: getRandomColor(9)
   },
   {
     id: 10,
     value: 'C',
     row: 2,
     column: 2,
-    color: getRandomColor()
+    color: getRandomColor(10)
   },
   {
     id: 11,
     value: 'V',
     row: 2,
     column: 3,
-    color: getRandomColor()
+    color: getRandomColor(11)
   },
   {
     id: 12,
     value: 'T',
     row: 3,
     column: 0,
-    color: getRandomColor()
+    color: getRandomColor(12)
   },
   {
     id: 13,
     value: 'I',
     row: 3,
     column: 1,
-    color: getRandomColor()
+    color: getRandomColor(13)
   },
   {
     id: 14,
     value: 'P',
     row: 3,
     column: 2,
-    color: getRandomColor()
+    color: getRandomColor(14)
   },
   {
     id: 15,
     value: 'E',
     row: 3,
     column: 3,
-    color: getRandomColor()
+    color: getRandomColor(15)
+  },
+  {
+    id: 16,
+    value: 'A',
+    row: 4,
+    column: 0,
+    color: getRandomColor(16)
+  },
+  {
+    id: 17,
+    value: 'H',
+    row: 4,
+    column: 1,
+    color: getRandomColor(17)
+  },
+  {
+    id: 18,
+    value: 'E',
+    row: 4,
+    column: 2,
+    color: getRandomColor(18)
+  },
+  {
+    id: 19,
+    value: 'U',
+    row: 4,
+    column: 3,
+    color: getRandomColor(19)
+  },
+  {
+    id: 20,
+    value: 'C',
+    row: 5,
+    column: 0,
+    color: getRandomColor(20)
+  },
+  {
+    id: 21,
+    value: 'O',
+    row: 5,
+    column: 1,
+    color: getRandomColor(21)
+  },
+  {
+    id: 22,
+    value: 'H',
+    row: 5,
+    column: 2,
+    color: getRandomColor(22)
+  },
+  {
+    id: 23,
+    value: 'R',
+    row: 5,
+    column: 3,
+    color: getRandomColor(23)
+  }
+];
+var data2 = [
+  {
+    id: 0,
+    value: 'W',
+    row: 0,
+    column: 0,
+    color: getRandomColor(0)
+  },
+  {
+    id: 1,
+    value: 'O',
+    row: 0,
+    column: 1,
+    color: getRandomColor(1)
+  },
+  {
+    id: 2,
+    value: 'T',
+    row: 0,
+    column: 2,
+    color: getRandomColor(2)
+  },
+  {
+    id: 3,
+    value: 'Qu',
+    row: 0,
+    column: 3,
+    color: getRandomColor(3)
+  },
+  {
+    id: 4,
+    value: 'L',
+    row: 1,
+    column: 0,
+    color: getRandomColor(4)
+  },
+  {
+    id: 5,
+    value: 'S',
+    row: 1,
+    column: 1,
+    color: getRandomColor(5)
+  },
+  {
+    id: 6,
+    value: 'R',
+    row: 1,
+    column: 2,
+    color: getRandomColor(6)
+  },
+  {
+    id: 7,
+    value: 'A',
+    row: 1,
+    column: 3,
+    color: getRandomColor(7)
+  },
+  {
+    id: 8,
+    value: 'Y',
+    row: 2,
+    column: 0,
+    color: getRandomColor(8)
+  },
+  {
+    id: 9,
+    value: 'E',
+    row: 2,
+    column: 1,
+    color: getRandomColor(9)
+  },
+  {
+    id: 10,
+    value: 'C',
+    row: 2,
+    column: 2,
+    color: getRandomColor(10)
+  },
+  {
+    id: 11,
+    value: 'V',
+    row: 2,
+    column: 3,
+    color: getRandomColor(11)
+  },
+  {
+    id: 15,
+    value: 'E',
+    row: 3,
+    column: 3,
+    color: getRandomColor(15)
+  },
+  {
+    id: 16,
+    value: 'A',
+    row: 3,
+    column: 0,
+    color: getRandomColor(16)
+  },
+  {
+    id: 17,
+    value: 'H',
+    row: 3,
+    column: 1,
+    color: getRandomColor(17)
+  },
+  {
+    id: 18,
+    value: 'E',
+    row: 3,
+    column: 2,
+    color: getRandomColor(18)
+  },
+  {
+    id: 19,
+    value: 'U',
+    row: 4,
+    column: 3,
+    color: getRandomColor(19)
+  },
+  {
+    id: 20,
+    value: 'C',
+    row: 4,
+    column: 0,
+    color: getRandomColor(20)
+  },
+  {
+    id: 21,
+    value: 'O',
+    row: 4,
+    column: 1,
+    color: getRandomColor(21)
+  },
+  {
+    id: 22,
+    value: 'H',
+    row: 4,
+    column: 2,
+    color: getRandomColor(22)
+  },
+  {
+    id: 23,
+    value: 'R',
+    row: 5,
+    column: 3,
+    color: getRandomColor(23)
+  },
+  {
+    id: 24,
+    value: 'S',
+    row: 5,
+    column: 0,
+    color: getRandomColor(24)
+  },
+  {
+    id: 25,
+    value: 'I',
+    row: 5,
+    column: 1,
+    color: getRandomColor(25)
+  },
+  {
+    id: 26,
+    value: 'A',
+    row: 5,
+    column: 2,
+    color: getRandomColor(26)
   }
 ];
 
 chart.draw(data);
+
+setTimeout(function() {
+  chart.draw(data2);
+}, 1000);
 
 setupScores();
 
@@ -181,11 +412,14 @@ function getPointsForWord(word) {
   return points;
 }
 
-function getRandomColor() {
+function getRandomColor(id) {
   var colors = ['#F75168', '#678FC1', '#b5577e', '#1DAD7C', '#F98D3F'];
   var randomIndex = Math.floor(Math.random() * colors.length);
 
-  return colors[randomIndex];
+  if (savedColors[id] === undefined) {
+    savedColors[id] = colors[randomIndex];
+  }
+  return savedColors[id];
 }
 
 function addToScore(newScore) {
