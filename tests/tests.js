65090aba9fca38298dceec677831fd4a73adb237
Adam Harris <aharris88@users.noreply.github.com>
2015-06-03 16:48:03 -0600

    Update isAWord test to be asynchronous

--- tests/tests.js
+++ tests/tests.js
@@ -142,12 +142,14 @@ describe("game", function() {
 		game.setData(data);
 		expect(game.isAWordLeft()).to.be.false;
 	});
-	it("should check if string is a word", function() {
+	it("should check if string is a word", function(done) {
 		var trueCallback = function(success) {
 			expect(success).to.be.true;
+			done();
 		};
 		var falseCallback = function(success) {
 			expect(success).to.be.false;
+			done();
 		};
 		game.isAWord('hello', trueCallback);
 		game.isAWord('asdf', falseCallback);
